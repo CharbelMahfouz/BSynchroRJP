fefb10032969c85f2de72fa14bdc2ef0256a8b71
CharbelMahfouz <charbel.mahfouz52@gmail.com>
2023-12-09 16:04:56 +0200

    created customer details endpoint

---
+++ API/Controllers/CustomersController.cs
@@ -0,0 +1,27 @@
+﻿using BLL.DTOs;
+using BLL.IServices;
+using BLL.Utilities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+
+    public class CustomersController : APIBaseController
+    {
+        private readonly ICustomersBL _customersService;
+
+        public CustomersController(ICustomersBL customersService)
+        {
+            _customersService = customersService;
+        }
+
+        [HttpGet("{customerId}")]
+        public async Task<IActionResult> GetCustomerDetails([FromRoute] int customerId)
+        {
+            var details = await _customersService.GetCustomerDetails(customerId);
+            return Ok(Helpers.CreateResponseModel(1, "Customer details", details));
+        }
+    }
+}
--- BLL/Utilities/Helpers.cs
+++ BLL/Utilities/Helpers.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Hosting;
+﻿using BLL.DTOs;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using RestSharp;
 using System;
@@ -14,6 +15,16 @@ namespace BLL.Utilities
     public static class Helpers
     {
         private static Random random = new Random();
+        public static ResponseModel CreateResponseModel(int result, string message, dynamic data)
+        {
+            ResponseModel responseModel = new ResponseModel()
+            {
+                Result = result,
+                Message = message,
+                Data = data
+            };
+            return responseModel;
+        }
         public static async Task<bool> SendEmailAsync(string toEmail, string subject, string content)
         {
             var client = new RestClient("https://api.sendinblue.com/v3/smtp/email");
@@ -49,76 +60,6 @@ namespace BLL.Utilities
             return strrandom;
         }
 
-
-        public static string RemoveCountryCode(string phoneNumber)
-        {
-            List<string> countries_list = new List<string>();
-            countries_list.Add("+966");
-            countries_list.Add("966");
-            countries_list.Add("961");
-            countries_list.Add("+961");
-            countries_list.Add("0");
-
-            foreach (var country in countries_list)
-            {
-                if (country == "0")
-                {
-                    if (phoneNumber.Substring(0, 1) == "0")
-                    {
-                        phoneNumber = phoneNumber.Remove(0, 1);
-                    }
-                }
-                else if (phoneNumber.StartsWith(country))
-                {
-                    phoneNumber = phoneNumber.Replace(country, "");
-                }
-            }
-
-            return phoneNumber;
-        }
-
-        public static bool SendSMS(string to, string content)
-        {
-            try
-            {
-                //var sssss = "https://sms.leblines.com/api.php?username=Fawwil&password=Fawwil9933&action=sendsms&from=Fawwil&to=" + to + "&text=" + content;
-                //string res = to.Substring(0, 1);
-
-                //string res = to.Substring(0, 3);
-
-                //if (res == "966")
-                //{
-                //    to = to.Remove(0, 3);
-                //    if (to.Substring(0, 1) == "0")
-                //    {
-                //        to = to.Remove(0, 1);
-                //    }
-                //} else if(to.Substring(0, 1) == "0")
-                //{
-                //    to = to.Remove(0, 1);
-                //}
-                to = RemoveCountryCode(to);
-                //var sssss = "https://sms.leblines.com/api.php?username=Fawwil&password=Fawwil9933&action=sendsms&from=Fawwil&to=961" + to + "&text=Please follow this link to place your payment " + content;
-                //var sssss = "https://sms.leblines.com/api.php?username=Fawwil&password=Fawwil9933&action=sendsms&from=Fawwil&to=961" + to + $"&text={content}";
-                //var sssss = "https://api.smsala.com/api/SendSMS?api_id=API898318662588&api_password=P@ssw0rd&sms_type=P&encoding=T&sender_id=HOLOL&phonenumber=966" + to + $"&textmessage={content}" + "&uid=SAU898312805752&callback_url=https://xyz.com/";
-                //var sssss = "http://sms.gateway.sa:6005/api/v2/SendSMS?SenderId=CaterMe&Is_Unicode=false&Is_Flash=false&" + $"Message={content}" + "&MobileNumbers=966" + to + "&ApiKey=CG6WXovTOdX12VOP+781SZR27ff6Pd6yCrZHf1UAvGc=&ClientId=d91477cf-98fe-4e2f-9e1e-533ae06ecb29";
-                var sssss = " https://www.proxireach.com/bestsmsbulkapi/sendSmsAPI.php?username=megabee&&password=MegaBee123!faq" + $"&&message={content}&&senderid=Brandsome&&destination=" + to /*+ "&&date=DATE&&time=TIME"*/;
-
-                var httpWebRequest = (HttpWebRequest)WebRequest.Create(sssss);
-                HttpWebResponse httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
-                //if (httpResponse.StatusDescription == "OK")
-                //{
-                //    return true;
-                //}
-                return false;
-            }
-            catch (Exception ex)
-            {
-                return false;
-                throw;
-            }
-
-        }
 
         public static string RandomStringNoCharacters(int length)
         {
