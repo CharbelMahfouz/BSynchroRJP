f952c3cc1f38e05574718640e0982fc22036b8c0
CharbelMahfouz <charbel.mahfouz52@gmail.com>
2023-12-09 14:57:57 +0200

    created open account endpoint

---
+++ API/Controllers/AccountsController.cs
@@ -0,0 +1,25 @@
+﻿using BLL.DTOs;
+using BLL.IServices;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+   
+    public class AccountsController : APIBaseController
+    {
+        private readonly IAccountsBL _accountsService;
+
+        public AccountsController(IAccountsBL accountsService)
+        {
+            _accountsService = accountsService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> OpenAccount(OpenAccountDTO model)
+        {
+            return Ok(await _accountsService.OpenAccount(model));
+        }
+    }
+}
